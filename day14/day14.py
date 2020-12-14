a45d145b46f472c3b56fa7c0cd148b3f46a809b2
Frank <Carver>
2020-12-14 20:54:10 +0000

    day 14 part 2 done

--- day14/day14.py
+++ day14/day14.py
@@ -1,7 +1,23 @@
 import re
+import math
 
 BITS = 36
 MAX = 0b111111111111111111111111111111111111
+
+def invert(x):
+    return MAX - x
+
+def double(values, bit):
+    print("double:",values,bit)
+    ret = []
+    for value in values:
+        print("considering:",value)
+        zero = value & invert(bit)
+        one = zero | bit
+        ret.append(zero)
+        ret.append(one)
+        print("appended:",ret)
+    return ret
 
 class Cpu:
     def __init__(self):
@@ -32,18 +48,40 @@ class Cpu:
 
     def apply_mask(self, value):
         a = self.enable & self.value
-        b = (MAX - self.enable) & value
+        b = (invert(self.enable)) & value
         ret = a | b
         # print("apply mask e:",self.enable,"v:",self.value,"a:",a,"b:",b,"->",ret)
         return ret
 
-    def set_value(self, addr, value):
-        masked = self.apply_mask(value)
-        # print("set:",value,'->',masked)
-        if masked > 0:
-            self.mem[addr] = masked
+    def apply_mask2(self, value):
+        floaters = invert(self.enable)
+        print("apply mask 2 e:",format(self.enable,'b'),"f:",format(floaters,'b'),"v:",self.value,"i",value)
+        base = self.value | value
+        ret = [base]
+        for i in range(BITS):
+            bit = int(math.pow(2,i))
+            if (floaters & bit):
+                ret = double(ret, bit)
+                print("after double:", ret)
+                # raise(Exception("huh"))
+        print("apply mask 2 e:",self.enable,"v:",self.value,"->",ret)
+        return ret
+
+    def put(self, addr, value):
+        # print("put:",addr,':=',value)
+        if value > 0:
+            self.mem[addr] = value
         else:
-            self.mem.pop(addr,None)
+            self.mem.pop(addr, None)
+
+    def set_value(self, addr, value, qpart):
+        if (qpart == 1):
+            masked = self.apply_mask(value)
+            self.put(addr, masked)
+        else:
+            masked = self.apply_mask2(addr)
+            for a in masked:
+                self.put(a, value)
         return masked
 
     def count_memory(self):
@@ -52,6 +90,9 @@ class Cpu:
             ret += self.mem[loc]
         return ret
 
+# return all possible combinations of bits where mask is 0
+def splurge(mask,value, input):
+    pass
 
 def solve(qpart, filename='input.txt'):
     print("Part " + str(qpart))
@@ -69,7 +110,7 @@ def solve(qpart, filename='input.txt'):
         possible = re.match("mem\[(\d+)\] = (\d+)", line)
         if possible:
             addr,value = possible.groups()
-            masked = cpu.set_value(addr,int(value))
+            masked = cpu.set_value(int(addr),int(value), qpart)
             print("set addr:",addr,"value:",value,"->",masked)
             continue
         print("unknown line: " + line)
@@ -79,6 +120,14 @@ def solve(qpart, filename='input.txt'):
 
 if __name__ == '__main__':
     # solve(1, "test1.txt")
-    solve(1)
+    # solve(1)
+
+    # cpu = Cpu()
+    # cpu.set_mask('000000000000000000000000000000000X0X')
+    # v = 0xF
+    # print('app1',cpu.apply_mask(v))
+    # print('app2',cpu.apply_mask2(v))
+    # solve(2, "test2.txt")
+    # solve(2, "test3.txt")
     # solve(2, "test1.txt")
-    # solve(2)
+    solve(2)
