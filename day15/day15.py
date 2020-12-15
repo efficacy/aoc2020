9e880c813974df3e707100d761618b1734f3bc89
Frank <Carver>
2020-12-15 10:57:05 +0000

    day 15 tidyups

--- day15/day15.py
+++ day15/day15.py
@@ -1,19 +1,4 @@
 
-def add(n, buf, turn):
-    sz = len(buf)
-    value = 0
-    found = False
-    for i in range(sz-1,0,-1):
-        index = i-1
-        if buf[index] == n:
-            value = sz - i
-            # print("turn",turn,"=",value,"(found",n,"at index",index,"of size",sz,")")
-            found = True
-            break
-    # if not found:
-    #     print("turn",turn,"=",value,"(not found", n, ")")
-    return value
-
 def next(n, map, turn):
     if n in map:
         value = turn - map[n] - 1
@@ -34,7 +19,7 @@ def solve(qpart, filename='input.txt', nsteps=None):
         lines = f.read().splitlines()
     starters = [int(x) for x in lines[0].split(',')]
     print("loaded:",starters)
-    # buf = []
+
     map = {}
     turn = 1
     prev = None
@@ -42,29 +27,24 @@ def solve(qpart, filename='input.txt', nsteps=None):
         if prev != None:
             map[prev] = turn-1
         print("turn",turn,"=",value,"(starter)")
-        # buf.append(value)
         turn += 1
         prev = value
-    # print(" buf:", buf)
-    # print(" map:", map)
+
     while turn <= nsteps:
         value = next(prev, map, turn)
         map[prev] = turn-1
-        # print(" buf:", buf)
-        # print(" map:",map)
-        # buf.append(value)
         turn += 1
         if 0 == turn % 1000:
             print(".",end='')
         if 0 == turn % 100000:
             print("\n",turn)
         prev = value
-    # print("buf:",buf)
-    # print("map:",map)
     print("final:",prev)
 
 if __name__ == '__main__':
+    # solve(1, "test1.txt", 10)
     # solve(1, "test1.txt")
     # solve(1)
     # solve(2, "test1.txt", 10)
+    # solve(2, "test1.txt")
     solve(2)
