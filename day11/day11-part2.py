af3022fae4a2eadc892e8112bb04d59ca668ea9c
Frank <Carver>
2020-12-15 19:25:37 +0000

    finally finished day 11 part 2

--- day11/day11-part2.py
+++ day11/day11-part2.py
@@ -1,51 +1,42 @@
+
+sign = lambda x: -1 if x < 0 else (1 if x > 0 else 0)
+
+direction_lookup = {
+    (-1, 0): 'n',
+    (-1, 1): 'ne',
+    (0, 1): 'e',
+    (1, 1): 'se',
+    (1, 0): 's',
+    (1, -1): 'sw',
+    (0, -1): 'w',
+    (-1, -1): 'nw'
+}
+
+def dump(seats,label, height, width):
+    print("----",label)
+    map = []
+    for r in range(height):
+        row = []
+        for c in range(width):
+            row.append(".")
+        map.append(row)
+
+    for seat in seats:
+        map[seat.r][seat.c] = seat.status
+
+    for r in range(height):
+        print("".join(map[r]))
+
 class Seat:
     def __init__(self, r, c, status):
         # print("creating seat r:",r,"c:",c)
         self.r = r
         self.c = c
         self.status = status
-        self.nb = {
-            'n': [],
-            'ne': [],
-            'e': [],
-            'se': [],
-            's': [],
-            'sw': [],
-            'w': [],
-            'nw':[]
-        }
         self.neighbours = []
 
     def is_occupied(self):
         return self.status == '#' or self.status == '^'
-
-    def is_adjacent(self, other):
-        if self.r == other.r and self.c == other.c:
-            return None # don't count myself!
-        diff_r = other.r - self.r
-        diff_c = other.c - self.c
-        if diff_r == 0:
-            if diff_c > 0:
-                return 'e'
-            elif diff_c < 0:
-                return 'w'
-        elif diff_c == 0:
-            if diff_r < 0:
-                return 'n'
-            elif diff_r > 0:
-                return 's'
-        elif abs(diff_c) == abs(diff_r):
-            if diff_c > 0:
-                if diff_r > 0:
-                    return 'ne'
-                else:
-                    return 'se'
-            elif diff_c < 0:
-                if diff_r > 0:
-                    return 'nw'
-                else:
-                    return 'sw'
-        return None
 
     def count_neighbours(self):
         n = 0
@@ -60,13 +51,37 @@ class Seat:
                 return True
         return False
 
+    def find_direction(self, other):
+        if self.r == other.r and self.c == other.c:
+            return (None,None) # don't count myself!
+        # now work out if the supplied co-ords lie on a cardinal axis from this point
+        offset_r = other.r - self.r
+        abs_r = abs(offset_r)
+        offset_c = other.c - self.c
+        abs_c = abs(offset_c)
+
+        if  offset_r == 0 or offset_c == 0 or abs_c == abs_r:
+            if offset_r == 0:
+                offset = (0, sign(offset_c))
+                distance = abs_c
+            elif offset_c == 0:
+                offset = (sign(offset_r), 0)
+                distance = abs_r
+            else:
+                offset = ( sign(offset_r), sign(offset_c))
+                distance = abs_r
+
+            return (direction_lookup[offset], distance)
+
+        return (None,None)
+
     def occupy(self):
         # print("occupy",self.r,self.c)
         self.status = 'v' # v => sitting down
 
     def vacate(self):
         # print("vacate",self.r,self.c)
-        self.status = '^' # v => standing up
+        self.status = '^' # ^ => standing up
 
     def complete(self):
         if self.status == 'v':
@@ -80,17 +95,24 @@ class Seat:
 def generation(seats):
     changed = False
     for seat in seats:
+        # watch = (seat.r == 0)
+        # if watch:
+        #     print(seat,end="=")
+        #     for n in seat.neighbours:
+        #         print(n,end=',')
+        #     print()
+        old = seat.status
         occupied = seat.is_occupied()
-        n = seat.count_neighbours()
-        # print("considering:",seat.r,seat.c,"occ:",occupied,"n:",n)
         if not occupied:
             if not seat.has_neighbours():
                 seat.occupy()
                 changed = True
         else:
-            if seat.count_neighbours() == 5:
+            if seat.count_neighbours() >= 5:
                 seat.vacate()
                 changed = True
+        # if watch:
+        #     print(" ", old,'->',seat.status)
     if changed:
         for seat in seats:
             seat.complete()
@@ -99,43 +121,66 @@ def generation(seats):
 def manhattan(seat1, seat2):
     return int(abs(seat2.r-seat1.r) + abs(seat2.c-seat1.c))
 
-width = 0
-height = 0
+def find_directional_neighbours(seat, seats):
+    # print("fdn considering",seat)
+    ret = { 'n':(None,None),'ne':(None,None),'e':(None,None),'se':(None,None),
+            's':(None,None),'sw':(None,None),'w':(None,None),'nw':(None,None), }
+    for other in seats:
+        direction,distance = seat.find_direction(other)
+        if direction != None:
+            # print(" found neighbour dir:",direction,"dist:",distance)
+            best = ret[direction][0]
+            if best == None or distance < best:
+                # print("updated best",direction,"to",seat)
+                ret[direction] = (distance, other)
 
-def solve(qpart, filename='input.txt', size=25):
-    print("Part " + str(qpart))
+    # print("seat",seat,"has neighbours")
+    # for d in ret:
+    #     dist, seat = ret[d]
+    #     if dist != None:
+    #         print(' ',d,seat,dist)
+    return ret
+
+def init_seats(seat, seats):
+    dirs = find_directional_neighbours(seat,seats)
+    for d in dirs:
+        entry = dirs[d]
+        # print("trying to unpack",entry)
+        dist,other = entry
+        if dist != None:
+            seat.neighbours.append(other)
+
+def load(filename):
+    height = 0
+    width = 0
     with open(filename, 'r') as f:
         lines = f.read().splitlines()
-
     seats = []
-    nseats = 0
-    for r in range(0,len(lines)):
+    for r in range(0, len(lines)):
         line = lines[r]
-        width = len(line)
-        ++height
-        for c in range(0,width):
+        width = max(width,len(line))
+        height += 1
+        for c in range(0, width):
             v = line[c]
             if v == 'L':
                 seats.append(Seat(r, c, 'L'))
-                nseats += 1
     for seat in seats:
-        for other in seats:
-            dir = seat.is_adjacent(other)
-            if dir:
-                distance = manhattan(seat, other)
-                seat.nb[dir].append((distance, other))
-        for dir in seat.nb:
-            if len(seat.nb[dir]) > 0:
-                list = sorted(seat.nb[dir], key=lambda tup: tup[0])
-                list.sort(key=lambda tup: tup[0])
-                seat.neighbours.append(list[0][1])
+        init_seats(seat, seats)
+    return (seats,height,width)
 
-    dump(seats,"0")
-    generation(seats)
-    dump(seats,"1")
-    generation(seats)
-    dump(seats,"2")
-    return
+
+def solve(qpart, filename='input.txt', size=25):
+    print("Part " + str(qpart))
+    seats,height,width = load(filename)
+
+    dump(seats,"0", height, width)
+    # generation(seats)
+    # dump(seats,"1", height, width)
+    # generation(seats)
+    # dump(seats,"2", height, width)
+    # generation(seats)
+    # dump(seats,"3", height, width)
+    # return
 
     step = 0
     while generation(seats):
@@ -147,8 +192,8 @@ def solve(qpart, filename='input.txt', size=25):
     for seat in seats:
         if seat.is_occupied():
             occupied += 1
-    print("terminated after",step,"steps with",occupied,"seats occupied")
+    print("\nterminated after",step,"steps with",occupied,"seats occupied")
 
 if __name__ == '__main__':
-    solve(2, "test1.txt")
-    # solve(2)
+    # solve(2, "test1.txt")
+    solve(2)
