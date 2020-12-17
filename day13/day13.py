1717828d35e097a5a8a9c4d4997c37bd717f10cd
Frank <Carver>
2020-12-17 14:47:03 +0000

    finally got to the bottom of day13 part 2
    The practical ones are easy,
    the maths ones show how little maths I know :(

--- day13/day13.py
+++ day13/day13.py
@@ -6,65 +6,42 @@ def find(possible):
     return None
 
 def solve(qpart, filename='input.txt'):
-    print("Part " + str(qpart))
+    print("Part",qpart)
     with open(filename, 'r') as f:
         lines = f.read().splitlines()
     now = int(lines[0])
     timetable = lines[1].split(',')
-    print(now,timetable)
+    # print(now,timetable)
     busses = []
     for i in range(len(timetable)):
         bus = timetable[i]
         if bus != 'x':
             busses.append((i, int(bus)))
-    print(now, busses)
+    # print(now, busses)
     if qpart == 1:
         then = now
-    else:
-        then = 0
-        state = 0
-        prefix = ''
-        nbusses = len(timetable)
-        start = None
-    while True:
-        if qpart == 1:
+        while True:
             possible = [then % x[1] for x in busses]
-            print(then,possible)
+            # print(then,possible)
             id = find(possible)
             if id != None:
                 break
             then += 1
-        else:
-            bus = timetable[state]
-            if bus != 'x':
-                mins = then % int(bus)
-                # print('state',state,'consi dering bus',bus,then,"mins",mins)
-                if mins == 0:
-                    # print("match at state",state)
-                    # print(".",end='')
-                    if state > 5:
-                        print(prefix+str(then))
-                    if state == 0:
-                        start = then
-                    state += 1
-                    prefix += ' '
-                else:
-                    state = 0
-                    prefix = ''
-            else:
-                state += 1
-            if state == nbusses:
-                print("got one at",start,"!")
-                break
-            then += 1
-            # steps += 1
-            # if steps == 20:
-            #     print("break out")
-            #     return
-
+        wait = then - now
+        return wait * busses[id][1]
+    else:
+        then = 0
+        step = 1
+        # print("busses", busses)
+        for mins, id in busses:
+            while (then + mins) % id != 0:
+                then += step
+            step *= id
+        return then
 
 if __name__ == '__main__':
-    # solve(1, "test1.txt")
-    # solve(1)
-    # solve(2, "test1.txt")
-    solve(2)
+    # answer = solve(1,"test1.txt")
+    # answer = solve(1)
+    # answer = solve(2,"test1.txt")
+    answer = solve(2)
+    print("answer",answer)
